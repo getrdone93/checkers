535eb541be75430c262de264e6d3170e73a5dff3
tanderson <treveranderson@unomaha.edu>
2017-11-14 19:09:47 -0600

    changed squares to maps

--- src/checkers/core.clj
+++ src/checkers/core.clj
@@ -1,20 +1,13 @@
 (ns checkers.core)
 
-;define board. true means checker is occupying space and false means nothing is there. 
-(def board [[false true false true false true false true] 
-            [true false true false]])
-
-
-;lets write a function to define the board instead of statically defning it
-(def board [m n] 
-  (genBoard m 0 n false []))
-
+;lets write a function to define the board instead of statically defning it. true means
+;playable square, false means otherwise.  
 (defn genBoard [m mct n occupy res] 
   (if (< mct m)
     (genBoard m (inc mct) n (not occupy) 
               (conj res ((fn genRow [n count occupy row] (if (< count n)
-                                                            (genRow n (inc count) (not occupy) (assoc row count occupy))
-                                                            row)) n 0 occupy [])))
+                                          (genRow n (inc count) (not occupy) (conj row (assoc (assoc {} :playable occupy) :pieceHere occupy)))
+                                                                    row)) n 0 occupy [])))
     res
     )
   )

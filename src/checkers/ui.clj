72a8b2fdb5c0e3e1147470d4d771cdbfd0207e92
tanderson <treveranderson@unomaha.edu>
2018-02-19 20:18:05 -0600

    got the checkerboard printed

--- src/checkers/ui.clj
+++ src/checkers/ui.clj
@@ -5,35 +5,37 @@
  '(java.awt.image BufferedImage)
  '(javax.swing JPanel JFrame))
 
-(def scale 25)
+(def scale 100)
 (def dim 80)
+(def num-squares 64)
 
 (def img (new BufferedImage (* scale dim) (* scale dim) 
                  (. BufferedImage TYPE_INT_ARGB)))
 
 (defn colorFrame [img g] 
   (let [imGraph (. img (getGraphics))]
-    (doto imGraph
-      (.setColor (. Color white))
-      (.fillRect 0 0 (. img (getWidth)) (. img (getHeight)))
-      (.setColor (. Color red))
-      (.fillRect 0 0 scale scale)
-      (.setColor (. Color black))
-      (.fillRect scale scale scale scale)
-;      (#(for [x (range 8) y (range 8)] 
-;         (do 
-;           (.setColor % (if (= (mod (+ x y) 2) 0)
-;                          (. Color red)
-;                          (. Color black)))
-;           (.fillRect % 0 0 scale scale))
-;         ))
-      )
-        
-    
+    (do 
+      (.setColor imGraph (. Color white))
+      (.fillRect imGraph 0 0 (. img (getWidth)) (. img (getHeight)))
+      (dotimes [n num-squares]
+          (let [place (mod n 8)
+                r (quot n 8)
+                x (if (zero? place)
+                    0
+                    (* place scale))
+                y (* r scale)
+                c (if (zero? (mod r 2))
+                    (if (zero? (mod n 2))
+                      (. Color red)
+                      (. Color black))
+                    (if (zero? (mod n 2))
+                      (. Color black)
+                      (. Color red)))
+                ]
+            (.setColor imGraph c)
+            (.fillRect imGraph x y scale scale))))
     (. g (drawImage img 0 0 nil))
     (. imGraph (dispose))))
-
-
 
 ;proxy implements/extends a interface/class where the supplied arguments
 ;are arguments to the class's super constructor and then calls

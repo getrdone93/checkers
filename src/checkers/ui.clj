9ec9aa02b4df4c66708cd54f472d19686e4d639d
tanderson <treveranderson@unomaha.edu>
2018-02-19 18:29:57 -0600

    getting closer to drawing a grid

--- src/checkers/ui.clj
+++ src/checkers/ui.clj
@@ -1,22 +1,47 @@
 (ns ui.core)
 
 (import 
- '(java.awt Color Graphics Dimension)
+ '(java.awt Color Graphics Dimension BorderLayout)
  '(java.awt.image BufferedImage)
  '(javax.swing JPanel JFrame))
 
-(def scale 5)
+(def scale 25)
+(def dim 80)
 
 (def img (new BufferedImage (* scale dim) (* scale dim) 
                  (. BufferedImage TYPE_INT_ARGB)))
 
-(def panel (doto (proxy [JPanel] []
-                        (paint [g] img))
-             (.setPreferredSize (new Dimension 
-                                     (* scale dim) 
-                                     (* scale dim)))))
+(defn colorFrame [img g] 
+  (let [imGraph (. img (getGraphics))]
+    (doto imGraph
+      (.setColor (. Color white))
+      (.fillRect 0 0 (. img (getWidth)) (. img (getHeight)))
+      (.setColor (. Color red))
+      (.fillRect 0 0 scale scale)
+      (.setColor (. Color black))
+      (.fillRect scale scale scale scale)
+;      (#(for [x (range 8) y (range 8)] 
+;         (do 
+;           (.setColor % (if (= (mod (+ x y) 2) 0)
+;                          (. Color red)
+;                          (. Color black)))
+;           (.fillRect % 0 0 scale scale))
+;         ))
+      )
+        
+    
+    (. g (drawImage img 0 0 nil))
+    (. imGraph (dispose))))
 
-;how to draw a grid on the frame?
-;i think i just need to update the img with a grid of squares
+
+
+;proxy implements/extends a interface/class where the supplied arguments
+;are arguments to the class's super constructor and then calls
+;the supplied functions
+(def panel (doto (proxy [JPanel] [(new BorderLayout)]
+                        (paint [g] (colorFrame img g)))
+             (.setPreferredSize (new Dimension 
+                                     (/ (* scale dim) 5) 
+                                     (/ (* scale dim) 5)))))
 
 (defn frame [] (doto (new JFrame) (.add panel) .pack .show))

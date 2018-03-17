ab06c4643c77ffd7716c688287b6167d155248ba
tanderson <treveranderson@unomaha.edu>
2018-03-16 23:34:34 -0500

    color-frame blows up stack but now is eggsicuted

--- src/checkers/ui.clj
+++ src/checkers/ui.clj
@@ -20,39 +20,39 @@
 (def img (new BufferedImage (* scale dim) (* scale dim) 
                  (. BufferedImage TYPE_INT_ARGB)))
 
-(defn colorFrame [img g] 
-  (let [imGraph (. img (getGraphics))
-        t1Rows (set (range 0 3))
-        t2Rows (set (range 5 8))
-        circRows (set (clojure.set/union t1Rows t2Rows))
-        circDim (/ scale 2)
-        shift (/ circDim 2)]
-    (.setColor imGraph (. Color white))
-    (.fillRect imGraph 0 0 (. img (getWidth)) (. img (getHeight)))
-    (dotimes [n num-squares]
-      (let [place (mod n 8)
-            r (quot n 8)
-            x (if (zero? place)
-                0
-                (* place scale))
-            y (* r scale)
-            c (if (zero? (mod r 2))
-                (if (zero? (mod n 2))
-                  (. Color red)
-                  (. Color black))
-                (if (zero? (mod n 2))
-                  (. Color black)
-                  (. Color red)))]
-        (.setColor imGraph c)
-        (.fillRect imGraph x y scale scale)
-        (when (and (contains? circRows r) (= c (. Color black)))
-          (do
-            (.setColor imGraph (if (contains? t1Rows r)
-                                 t1-color
-                                 t2-color))
-            (.fillOval imGraph (+ x shift) (+ y shift) circDim circDim)))))
-    (. g (drawImage img 0 0 nil))
-    (. imGraph (dispose))))
+;(defn colorFrame [img g] 
+;  (let [imGraph (. img (getGraphics))
+;        t1Rows (set (range 0 3))
+;        t2Rows (set (range 5 8))
+;        circRows (set (clojure.set/union t1Rows t2Rows))
+;        circDim (/ scale 2)
+;        shift (/ circDim 2)]
+;    (.setColor imGraph (. Color white))
+;    (.fillRect imGraph 0 0 (. img (getWidth)) (. img (getHeight)))
+;    (dotimes [n num-squares]
+;      (let [place (mod n 8)
+;            r (quot n 8)
+;            x (if (zero? place)
+;                0
+;                (* place scale))
+;            y (* r scale)
+;            c (if (zero? (mod r 2))
+;                (if (zero? (mod n 2))
+;                  (. Color red)
+;                  (. Color black))
+;                (if (zero? (mod n 2))
+;                  (. Color black)
+;                  (. Color red)))]
+;        (.setColor imGraph c)
+;        (.fillRect imGraph x y scale scale)
+;        (when (and (contains? circRows r) (= c (. Color black)))
+;          (do
+;            (.setColor imGraph (if (contains? t1Rows r)
+;                                 t1-color
+;                                 t2-color))
+;            (.fillOval imGraph (+ x shift) (+ y shift) circDim circDim)))))
+;    (. g (drawImage img 0 0 nil))
+;    (. imGraph (dispose))))
 
 (defn gen-board [n br]
                 (let [place (mod n 8)
@@ -73,7 +73,7 @@
                                                            :color c
                                                            :valid-click-locs []}
                                                   :checker {:point [(+ x shift) (+ y shift)]
-                                                            :color (if (contains? t1Rows r)
+                                                            :color (if (contains? t1-rows r)
                                                                      t1-color
                                                                      t2-color)}}))
                     br)))
@@ -84,20 +84,20 @@
   (let [im-graph (. img (getGraphics))]
        (.setColor im-graph (. Color white))
        (.fillRect im-graph 0 0 (. img (getWidth)) (. img (getHeight)))
-       #((fn draw-board [b]
-          (let [square ((first board) :square)
-                checker ((first board) :checker)
-                cx (first (checker :point))
-                cy (second (checker :point))
-                sqx (first (square :point))
-                sqy (second (square :point))]
-	          (when (not (empty? b))     
-	            (.setColor im-graph (((first board) :square) :color))
-              (.fillRect im-graph sqx sqy scale scale)
-              (when (= (square :color) (. Color black))
-                (.setColor im-graph (checker :color))
-                (.fillOval im-graph cx cy circ-dim circ-dim)))
-           (draw-board (rest b)))) board)
+       ((fn draw-board [b]
+         (let [square ((first board) :square)
+               checker ((first board) :checker)
+               cx (first (checker :point))
+               cy (second (checker :point))
+               sqx (first (square :point))
+               sqy (second (square :point))]
+	         (when (not (empty? b))     
+            (.setColor im-graph (((first board) :square) :color))
+            (.fillRect im-graph sqx sqy scale scale)
+            (when (= (square :color) (. Color black))
+              (.setColor im-graph (checker :color))
+              (.fillOval im-graph cx cy circ-dim circ-dim)))
+          (draw-board (rest b)))) board)
        (. g (drawImage img 0 0 nil))
        (. im-graph (dispose))))
 

31e5e9729affcbe357a73db415fa90bdd597bb22
tanderson <treveranderson@unomaha.edu>
2018-03-18 17:30:29 -0500

    data/drawing split out is complete!

--- src/checkers/ui.clj
+++ src/checkers/ui.clj
@@ -20,40 +20,6 @@
 (def img (new BufferedImage (* scale dim) (* scale dim) 
                  (. BufferedImage TYPE_INT_ARGB)))
 
-;(defn colorFrame [img g] 
-;  (let [imGraph (. img (getGraphics))
-;        t1Rows (set (range 0 3))
-;        t2Rows (set (range 5 8))
-;        circRows (set (clojure.set/union t1Rows t2Rows))
-;        circDim (/ scale 2)
-;        shift (/ circDim 2)]
-;    (.setColor imGraph (. Color white))
-;    (.fillRect imGraph 0 0 (. img (getWidth)) (. img (getHeight)))
-;    (dotimes [n num-squares]
-;      (let [place (mod n 8)
-;            r (quot n 8)
-;            x (if (zero? place)
-;                0
-;                (* place scale))
-;            y (* r scale)
-;            c (if (zero? (mod r 2))
-;                (if (zero? (mod n 2))
-;                  (. Color red)
-;                  (. Color black))
-;                (if (zero? (mod n 2))
-;                  (. Color black)
-;                  (. Color red)))]
-;        (.setColor imGraph c)
-;        (.fillRect imGraph x y scale scale)
-;        (when (and (contains? circRows r) (= c (. Color black)))
-;          (do
-;            (.setColor imGraph (if (contains? t1Rows r)
-;                                 t1-color
-;                                 t2-color))
-;            (.fillOval imGraph (+ x shift) (+ y shift) circDim circDim)))))
-;    (. g (drawImage img 0 0 nil))
-;    (. imGraph (dispose))))
-
 (defn gen-board [n br]
                 (let [place (mod n 8)
                       r (quot n 8)
@@ -73,9 +39,10 @@
                                                            :color c
                                                            :valid-click-locs []}
                                                   :checker {:point [(+ x shift) (+ y shift)]
-                                                            :color (if (contains? t1-rows r)
-                                                                     t1-color
-                                                                     t2-color)}}))
+                                                            :color (cond 
+                                                                     (contains? t1-rows r) t1-color
+                                                                     (contains? t2-rows r) t2-color
+                                                                     :else nil)}}))
                     br)))
 
 (def board (gen-board 0 []))
@@ -94,7 +61,7 @@
                sqy (second (square :point))]
               (.setColor im-graph (square :color))
               (.fillRect im-graph sqx sqy scale scale)
-              (when (= (square :color) (. Color black))
+              (when (and (= (square :color) (. Color black)) (some? (checker :color)))
                 (.setColor im-graph (checker :color))
                 (.fillOval im-graph cx cy circ-dim circ-dim)))
             (draw-board (rest b)))) board)

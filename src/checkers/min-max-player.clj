52dfe526da3b3bdace5a85e5da58156d2dbc3167
tanderson <treveranderson01@gmail.com>
2018-09-04 10:14:24 -0500

    got rid of code and removed func name

--- src/checkers/min-max-player.clj
+++ src/checkers/min-max-player.clj
@@ -9,28 +9,12 @@
       (= over other-team) -1
       (= over :tie) 0)))
 
-;; (defn max [[[[ind {{{t :team} :checker} :as entry}] paths] :as mov-chks] {la :last-action
-;;                                                                           s :state} other-team]
-;;   (let [utility (evaluation t other-team s)]
-;;     (if (contains? #{0 -1 1} utility)
-;;       {:last-action la :state s :utility utility}
-;;       )))
-
-;; (defn take-action [state {[ci chk] :move-checker sp :simple-paths ajp :all-jump-paths}]
-;;   (cond
-;;     (and (some? sp) (some? ajp)) nil bad input
-;;     (some? sp) (mapv (fn [mv] 
-;;                            (let [{rb :read-board ne :new-entry} (move-checker {:from [ci chk] :to mv})]
-;;                              ((king-me ne rb) :board))) sp)
-;;     (some? ajp) ((fn enumerate-jumps [[{p :path ns :next} :as path] res]
-;;                     ) ajp [])))
-
 (defn exec-jump [{from :from to :to :as move-form} jumped-chk read-board]
      (let [{mb :read-board ne :new-entry} (move-checker move-form (remove-checker jumped-chk read-board))]
        ((king-me ne mb) :board)))
 
 (defn dfs [ajp ajp-i read-board res]
-  ((fn recur-ns [jps ajp-ind b curr-ns r]
+  ((fn [jps ajp-ind b curr-ns r]
      (let [{p :path cns :next} (jps ajp-ind)
            [sci _] (last p)
            nji (first (difference cns curr-ns))]
@@ -44,4 +28,6 @@
                nb (exec-jump {:from board-sc :to board-mc} jc b)]
           (recur jps ajp-ind b (conj curr-ns nji) (into r (dfs jps nji nb r))))))) ajp ajp-i read-board #{} res))
 
-;(checkers.min-max-player/dfs ((second (first two-jump-vm)) :all-jump-paths) 0 @board [])
+; (def dfs-test (map (fn [[chk {ajp :all-jump-paths}]]
+;                    (checkers.min-max-player/dfs ajp 0 four-jump-board [])) four-jump-vm))
+            
